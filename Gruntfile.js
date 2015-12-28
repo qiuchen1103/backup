f4d46075a7a376c22f89d721fac24e5c130a0941
qiuchen1103 <qiuchen1103@qq.com>
2015-12-28 15:38:54 +0800

    resetGrunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,32 @@ module.exports = function(grunt) {
     uglify: {
       compressjs: {
         files: {
-          'js/main.min.js' : ['js/main.js']
+          'js/main.min.js' : ['workPlace/main.js']
         }
       }
     },
+    cssmin: {
+        compress: {
+          files: {
+            "css/FAQ.min.css": ["workPlace/FAQ.css"]
+          }
+        }
+      },
+
+    // 复制任务
+    copy: {
+       main: {
+          files: [
+            // {src: ['path/*'], dest: 'dest/', filter: 'isFile'}, // 复制path目录下的所有文件
+            {src: ['css/**','js/**','img/**','*.html'], dest: 'FAQ/'} // 复制path目录下的所有目录和文件
+          ]
+        }
+    },
+
     watch: {
       scripts: {
-        files: ['js/main.js'],
-        tasks: ['uglify']
+        files: ['workPlace/FAQ.css', 'workPlace/main.js'],
+        tasks: ['cssmin', 'uglify', 'copy']
       },
       livereload: {
           options: {
@@ -20,8 +38,8 @@ module.exports = function(grunt) {
           },
           files: [
               'index.html',
-              'css/FAQ.css',
-              'js/main.js'
+              'workPlace/FAQ.css',
+              'workPlace/main.js'
           ]
       }
     },
@@ -41,14 +59,14 @@ module.exports = function(grunt) {
       }
     }
   });
-
-  // grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
   
-  grunt.registerTask('watchit',['uglify','connect','watch']);
+  grunt.registerTask('watchit',['uglify','cssmin','copy','connect','watch']);
   grunt.registerTask('default');
 
 
